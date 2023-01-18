5e62e6aabf7fa318225b0861e186d86d2da5b0d0
Devon Dunham <itisdevondunham@gmail.com>
2023-01-18 10:28:28 -0500

    debugged

--- Capstone2023/Assets/Scripts/ProceduralGenerationAlgorithms.cs
+++ Capstone2023/Assets/Scripts/ProceduralGenerationAlgorithms.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public static class ProceduralGenerationAlgorithms
 {
-    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    public static HashSet<Vector2Int>
+    SimpleRandomWalk(Vector2Int startPosition, int walkLength)
     {
         HashSet<Vector2Int> path = new HashSet<Vector2Int>();
-        path.Add(startPosition);
+        path.Add (startPosition);
         var previousPosition = startPosition;
 
-        for(int i = 0; i < walkLength; i++)
+        for (int i = 0; i < walkLength; i++)
         {
-            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
-            path.Add(newPosition);
+            var newPosition =
+                previousPosition + Direction2D.GetRandomCardinalDirection();
+            path.Add (newPosition);
             previousPosition = newPosition;
         }
         return path;
@@ -22,16 +24,18 @@ public static class ProceduralGenerationAlgorithms
 
 public static class Direction2D
 {
-    public static List<Vector2Int> cardinalDirectionsList = new List<Vector2Int>();
-    {
-        new Vector2Int(0,1), // Up
-        new Vector2Int(1,0), //right
-        new Vector2Int(0,-1), //down
-        new Vector2Int(-1,0) //left
-    };
+    public static List<Vector2Int>
+        cardinalDirectionsList =
+            new List<Vector2Int> {
+                new Vector2Int(0, 1), // Up
+                new Vector2Int(1, 0), //right
+                new Vector2Int(0, -1), //down
+                new Vector2Int(-1, 0) //left
+            };
 
     public static Vector2Int GetRandomCardinalDirection()
     {
-        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+        return cardinalDirectionsList[Random
+            .Range(0, cardinalDirectionsList.Count)];
     }
 }
