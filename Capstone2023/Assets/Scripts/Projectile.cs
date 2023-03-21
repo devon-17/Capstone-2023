7ece478a57e457d3935bb938138a6a9ba19d22eb
Devon Dunham <itisdevondunham@gmail.com>
2023-03-21 11:04:11 -0400

    ongoing

--- Capstone2023/Assets/Scripts/Projectile.cs
+++ Capstone2023/Assets/Scripts/Projectile.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public GameObject player;
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        Vector3 targetDir = player.transform.position - transform.position;
-        transform.rotation = Quaternion.LookRotation(targetDir);
+        player = GameObject.FindWithTag("Player").transform;
+        Vector3 direction = player.position - transform.position;
+        direction.z = 0f; // lock x and y axis
+        if (direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation =
+                Quaternion.Euler(new Vector3(0f, 0f, angle + 90f));
+        }
+        //transform.rotation = Quaternion.LookRotation(targetDir);
 
+        /*
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation =
+            Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+        */
         //transform.Rotate(0, 0, transform.rotation.z);
-        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z);
     }
 
     // Update is called once per frame
