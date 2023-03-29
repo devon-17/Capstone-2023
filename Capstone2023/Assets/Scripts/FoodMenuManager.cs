e6777c13de4f1db46b1ae1bce93c309ed7c49c84
Devon Dunham <itisdevondunham@gmail.com>
2023-03-29 09:16:54 -0400

    ongoing

--- Capstone2023/Assets/Scripts/FoodMenuManager.cs
+++ Capstone2023/Assets/Scripts/FoodMenuManager.cs
@@ -5,22 +5,26 @@ using UnityEngine.UI;
 
 public class FoodMenuManager : MonoBehaviour
 {
-    public GameObject parentCanvas;
+    public Transform[] parent = new Transform[0];
+
     public GameObject[] menuItems = new GameObject[0];
+
     private Vector3 originalSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject menuChild = Instantiate(menuItems[0]);
-        // originalSize = menuChild.transform.scale;
-        menuChild.transform.parent = parentCanvas.transform;
-        //menuChild.transform.localScale = originalSize;
+        for (int i = 0; i <= menuItems.Length + 1; i++)
+        {
+            var randomItem = Random.Range(0, menuItems.Length);
+            GameObject menuChild = Instantiate(menuItems[randomItem]);
+            menuChild.transform.SetParent(parent[i]);
+            menuChild.transform.position = parent[i].position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
